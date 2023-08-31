cc7cd091d92f1788dc81f3a126bf8cb2c2c6a46f
Sheldon101 <53189556+Sheldon101@users.noreply.github.com>
2023-08-31 12:25:46 -0700

    Update keys.py
    got rid of keys

--- keys.py
+++ keys.py
@@ -1,21 +1,12 @@
-#this keys file should located in secret folder.
-#These keys are inactive !!!
-
+#These keys file should located in secret folder.
 #consumer_key=environ["API_KEY"]
 #consumer_secret=environ["API_SECRET"]                     
-
-#API_KEY=7PI9x7lKBPermlBqf0KkDEJq4
-#API_SECRET=pdhGhWynjjtY0HV49MDhUC6cJaqqt4rcSWvoxNvp6aO1Nkldps
-CONSUMER_KEY = '7PI9x7lKBPermlBqf0KkDEJq4'
-CONSUMER_SECRET ='pdhGhWynjjtY0HV49MDhUC6cJaqqt4rcSWvoxNvp6aO1Nkldps'
-
+CONSUMER_KEY = 'XXX'
+CONSUMER_SECRET ='XXXX'
 # bearer_token=environ["BEARER_TOKEN"],
-BEARER_TOKEN='AAAAAAAAAAAAAAAAAAAAAMlFmwEAAAAAXOXAOsS47CGncoOyCg1uTMrCKls%3Dq2KaJyVkjDXC2QhLxmdxINYYcMGgBtzYdLpgs8Q1kmnHXWZDYW'
-
-#ACCESS_TOKEN=1648372722446241794-3QYOPbL3lDOZ07pxnhKqFBxMogdVzr
-#ACCESS_TOKEN_SECRET=3KYrn54Tk6jR2pKbwPCI0kA45i0ESuelJpYfsILhd43K8
-ACCESS_KEY = '1648372722446241794-3QYOPbL3lDOZ07pxnhKqFBxMogdVzr'
-ACCESS_SECRET = '3KYrn54Tk6jR2pKbwPCI0kA45i0ESuelJpYfsILhd43K8'
+BEARER_TOKEN='XXX'
+ACCESS_KEY = 'XXX'
+ACCESS_SECRET = 'XXX'
 
 
 
